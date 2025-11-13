8c5c42206fe291b5d8a6fde41e964c7cec7a4e89
Asadullah khan <33312651+aukhan288@users.noreply.github.com>
2025-11-13 07:07:06 -0800

    commint

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import {useState} from "react"
-import { View} from "react-native"
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import AppNavigator from './src/navigation/AppNavigator';
 
-const App=()=>{
-  
-  return(
-   <View>
-
-   </View>
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
-}
+};
 
 export default App;
